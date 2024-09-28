b77b01f76ebeb855398e6c8f211eaf13028df1bf
micky0008 <micky0008@hotmail.fr>
2024-09-29 00:32:57 +0200

    meilleur test

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,26 @@ TFT_eSPI tft = TFT_eSPI(320, 240);
 
 void setup() {
   tft.init();
-  Serial.begin(57600); // For debug
+//  Serial.begin(57600); // For debug
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_YELLOW);
-  // tft.setFreeFont(&FreeSans12pt7b); //FreeSans12pt7b 29char / ligne  taille: 9 12 18 24
-  // tft.drawString("ta mere suces des bites en", 0, 0);
-  // tft.setFreeFont(&FreeSansBold18pt7b);
-  // tft.setTextColor(TFT_RED);
-  // tft.drawString("enfer", 0, 32);
+  //Pour les polices il y a 4 tailles => 9 12 18 24.
+  //pour FreeSans12pt7b ca fais environ 29char la ligne  en x
+  //pour y ca fais environ 20px.
   tft.setFreeFont(&FreeSans12pt7b);
 }
 
 int i = 0;
 void loop() {
-  tft.drawString("target X, distance: ", 0, 0);
-  tft.fillRect (220, 0, 100, 20, TFT_BLACK);
-  tft.drawNumber(i, 220, 0);
-  delay(2000);
+  int16_t x = tft.drawString("target 1, distance: ", 0, 0);
+  tft.fillRect(x + 5, 0, (320 - (x + 5)), 20, TFT_BLACK);
+  //tft.drawRect(x + 5, 0, (320 - (x + 5)), 20, TFT_CYAN);
+  tft.drawNumber(i, x + 5, 0);
+  x = tft.drawString("target 2, distance: ", 0, 25);
+  tft.fillRect(x + 5, 25, (320 - (x + 5)), 20, TFT_BLACK);
+  //tft.drawRect(x + 5, 25, (320 - (x + 5)), 20, TFT_CYAN);
+  tft.drawNumber(i, x + 5, 25);
+  delay(1000);
   i++;
 }
 
